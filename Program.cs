edc82f494b766bcc9cdafb10fa504997041a9d39
teddyzxcv <pan2691712848@gmail.com>
2020-11-14 11:44:28 +0300

    First commit for Newtonsoft

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AreaOfShape
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
